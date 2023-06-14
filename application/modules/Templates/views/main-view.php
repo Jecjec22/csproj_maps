b1df4f5581d7d83de57c9181d1c9d434f4599d66
Jecjec22 <jecrodriguez3@gmail.com>
2023-06-14 12:46:28 +0800

    Updated Code

--- application/modules/Templates/views/main-view.php
+++ application/modules/Templates/views/main-view.php
@@ -24,46 +24,44 @@
       <?php if($this->session->userdata('user_role') == 'Administrator'): ?>
 
         <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
-        
-        <li class="header">MAIN NAVIGATION</li>
-
-        <li><a href="<?php echo base_url('Dashboard'); ?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-        
-        <!-- <li class="header">LABELS</li> -->
-
-      </ul>
-
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-users"></i> <span>Manage Employee </span>
+        <ul class="sidebar-menu" data-widget="tree">
+    <li class="header">MAIN NAVIGATION</li>
+    
+    <li><a href="<?php echo base_url('Dashboard'); ?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+    
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-users"></i> <span>Manage Technician</span>
             <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
+                <i class="fa fa-angle-left pull-right"></i>
             </span>
-          </a>
-          <ul class="treeview-menu">
-            <li class=""><a href="<?php echo base_url('User/addUserPage'); ?>"><i class="fa fa-circle-o"></i> Add Employee </a></li>
-            <li class=""><a href="<?php echo base_url('User/viewUserPage'); ?>"><i class="fa fa-circle-o"></i> View All Employee </a></li>
-            <li class=""><a href="<?php echo base_url('User/makeUserSeniorPage'); ?>"><i class="fa fa-circle-o"></i> Make Employee's Supervisor </a></li>
-          </ul>
-        </li>
-        <!-- <li class="header">LABELS</li> -->
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="<?php echo base_url('User/addUserPage'); ?>"><i class="fa fa-circle-o"></i> Add Technician</a></li>
+            <li><a href="<?php echo base_url('User/viewUserPage'); ?>"><i class="fa fa-circle-o"></i> View All Technician</a></li>
+            
+        </ul>
+    </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-file"></i> <span>Schedule Tasks</span>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-file"></i> <span>Project and Scheduling</span>
             <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
+                <i class="fa fa-angle-left pull-right"></i>
             </span>
-          </a>
-          <ul class="treeview-menu">
-            <li class=""><a href="<?php echo base_url('ToDo/addToDoPage'); ?>"><i class="fa fa-circle-o"></i> Create ToDo Tasks</a></li>
-            <li class=""><a href="<?php echo base_url('ToDo/viewMyToDo'); ?>"><i class="fa fa-circle-o"></i> View My ToDo Tasks </a></li>
-            <li class=""><a href="<?php echo base_url('ToDo/addToDoJuniorPage'); ?>"><i class="fa fa-circle-o"></i> Assign Tasks </a></li>
-            <li class=""><a href="<?php echo base_url('ToDo/viewMyJuniorToDo'); ?>"><i class="fa fa-circle-o"></i> View Employee's Tasks </a></li>
-          </ul>
-        </li>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="<?php echo base_url('ToDo/addToDoPage'); ?>"><i class="fa fa-circle-o"></i> Add Project</a></li>
+            <li><a href="<?php echo base_url('ToDo/viewMyToDo'); ?>"><i class="fa fa-circle-o"></i> View Projects</a></li>
+            <li><a href="<?php echo base_url('ToDo/addToDoJuniorPage'); ?>"><i class="fa fa-circle-o"></i> Assign Project</a></li>
+            <li><a href="<?php echo base_url('ToDo/viewMyJuniorToDo'); ?>"><i class="fa fa-circle-o"></i> View Technician's Tasks</a></li>
+        </ul>
+    </li>
+    
+    <!-- New menu item for Projects -->
+   
+    
+</ul>
 
       </ul>
       <?php endif; ?>
@@ -86,7 +84,7 @@
           </a>
           <ul class="treeview-menu">
             <li class=""><a href="<?php echo base_url('ToDo/addToDoPage'); ?>"><i class="fa fa-circle-o"></i> Create ToDo Task </a></li>
-            <li class=""><a href="<?php echo base_url('ToDo/viewMyToDo'); ?>"><i class="fa fa-circle-o"></i> View My ToDo Tasks </a></li>
+            <li class=""><a href="<?php echo base_url('ToDo/viewMyToDo'); ?>"><i class="fa fa-circle-o"></i> View My Tasks </a></li>
           </ul>
         </li>
 
